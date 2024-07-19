89d48260434fa821431c02e145bda28f81571fa3
sucf <1058772978@qq.com>
2024-07-19 23:38:48 +0800

    init.

---
+++ src/main/java/com/dao/bi/controller/DataSourceController.java
@@ -0,0 +1,12 @@
+package com.dao.bi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: sucf
+ * @date: 2024/7/19 23:37
+ * @description:
+ */
+@RestController
+public class DataSourceController {
+}
---
+++ src/main/java/com/dao/bi/service/DataSourceService.java
@@ -0,0 +1,13 @@
+package com.dao.bi.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: sucf
+ * @date: 2024/7/19 23:35
+ * @description:
+ */
+@Service
+public class DataSourceService {
+
+}
